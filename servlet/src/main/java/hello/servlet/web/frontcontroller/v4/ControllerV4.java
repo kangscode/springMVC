481b63e432382fd13308d1bcf9f2232a3e8b16b4
kangscode <2009ksj@kau.kr>
2024-04-09 16:07:47 +0900

    mvc begin

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,15 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+
+    /**
+     *
+     * @param paraMap
+     * @param model
+     * @return viewname
+     */
+    String process(Map<String, String> paraMap, Map<String, Object> model);
+
+}
